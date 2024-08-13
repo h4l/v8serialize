699b10513b8cf5bf2a7c7901cd06ead19012a11a
Hal Blackburn <hwtb2@cam.ac.uk>
2024-08-13 07:47:12 +0000

    wip: feat: create sub methods for JSObject impl

--- src/v8serialize/jstypes/jsobject.py
+++ src/v8serialize/jstypes/jsobject.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import MutableMapping, TypeVar
+from dataclasses import dataclass
+from typing import Iterator, MutableMapping, TypeVar
+
+from v8serialize.jstypes.jsarrayproperties import ArrayProperties
 
 KT = TypeVar("KT", bound=int | str)
-T = TypeVar("T")
+VT = TypeVar("VT")
 
 
 MIN_SPARSE_ARRAY_SIZE = 16
@@ -12,7 +15,8 @@ MIN_DENSE_ARRAY_USED_RATIO = 1 / 4
 MAX_DENSE_ARRAY_HOLE_RATIO = 1 / 4
 
 
-class JSObject(MutableMapping[KT, T], ABC):
+@dataclass(slots=True)
+class JSObject(MutableMapping[KT, VT], ABC):
     """A Python model of JavaScript plain objects, limited to the behaviour that
     can be transferred with V8 serialization (which is essentially the behaviour
     of [`structuredClone()`]).
@@ -26,7 +30,28 @@ class JSObject(MutableMapping[KT, T], ABC):
     in order to serialize themselves as JavaScript Objects.
     """
 
-    # array_properties: ArrayProperties
+    array_properties: ArrayProperties[VT]
+    name_properties: dict[str, VT]
+
+    def __init__(self) -> None:
+        pass
+
+    # __getitem__, __setitem__, __delitem__, __iter__, __len__
+
+    def __getitem__(self, key: KT, /) -> VT:
+        raise NotImplementedError
+
+    def __setitem__(self, key: KT, value: VT, /) -> None:
+        raise NotImplementedError
+
+    def __delitem__(self, key: KT, /) -> None:
+        raise NotImplementedError
+
+    def __len__(self) -> int:
+        raise NotImplementedError
+
+    def __iter__(self) -> Iterator[KT]:
+        raise NotImplementedError
 
     # TODO: should we provide a specialised map that acts like JavaScript
     #   objects? e.g. allows for number or string keys that are synonyms. Plus
