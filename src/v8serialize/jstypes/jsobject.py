56ed0c3e3c10165b28f4e2cc22235521b07a4ec5
Hal Blackburn <hwtb2@cam.ac.uk>
2024-08-15 06:47:37 +0000

    feat: implement JSObject

--- src/v8serialize/jstypes/jsobject.py
+++ src/v8serialize/jstypes/jsobject.py
@@ -2,17 +2,24 @@ from __future__ import annotations
 
 from abc import ABC
 from dataclasses import dataclass
+from itertools import chain
 from typing import Iterator, MutableMapping, TypeVar
 
-from v8serialize.jstypes.jsarrayproperties import ArrayProperties
+from v8serialize.jstypes._normalise_property_key import normalise_property_key
+from v8serialize.jstypes.jsarrayproperties import (
+    ArrayProperties,
+    DenseArrayProperties,
+    JSHole,
+    SparseArrayProperties,
+)
 
 KT = TypeVar("KT", bound=int | str)
 VT = TypeVar("VT")
 
 
+# TODO: measure & adjust these
 MIN_SPARSE_ARRAY_SIZE = 16
 MIN_DENSE_ARRAY_USED_RATIO = 1 / 4
-MAX_DENSE_ARRAY_HOLE_RATIO = 1 / 4
 
 
 @dataclass(slots=True)
@@ -21,7 +28,8 @@ class JSObject(MutableMapping[KT, VT], ABC):
     can be transferred with V8 serialization (which is essentially the behaviour
     of [`structuredClone()`]).
 
-    [`structuredClone()`]: https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
+    [`structuredClone()`]: \
+https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
 
     The behaviour implemented aims to match that describe by the [ECMA-262] spec.
     [ECMA-262]: https://tc39.es/ecma262/#sec-object-type
@@ -30,30 +38,84 @@ class JSObject(MutableMapping[KT, VT], ABC):
     in order to serialize themselves as JavaScript Objects.
     """
 
-    array_properties: ArrayProperties[VT]
-    name_properties: dict[str, VT]
+    array: ArrayProperties[VT]
+    """Properties with integer names."""
+    properties: dict[str, VT]
+    """Properties with string names."""
 
     def __init__(self) -> None:
-        pass
-
-    # __getitem__, __setitem__, __delitem__, __iter__, __len__
+        self.array = DenseArrayProperties()
+        self.properties = {}
 
     def __getitem__(self, key: KT, /) -> VT:
-        raise NotImplementedError
+        k = normalise_property_key(key)
+        if type(k) is str:
+            properties = self.properties
+            if k in properties:
+                return properties[k]
+            raise KeyError(key)  # preserve the non-normalised key
+        else:
+            assert isinstance(k, int)
+            array_properties = self.array
+            if 0 <= k < len(array_properties):
+                value = array_properties[k]
+                if JSHole.isnot(value):
+                    return value
+            raise KeyError(key)  # preserve the non-normalised key
 
     def __setitem__(self, key: KT, value: VT, /) -> None:
-        raise NotImplementedError
+        k = normalise_property_key(key)
+        if type(k) is str:
+            self.properties[k] = value
+        else:
+            assert isinstance(k, int)
+            self._ensure_array_capacity(k)
+            self.array[k] = value
+
+    def _ensure_array_capacity(self, index: int) -> None:
+        array = self.array
+        length = len(array)
+        if index < length:
+            return
+        new_length = index + 1
+
+        # Swap the array properties implementation from dense to sparse to avoid
+        # wasting space for long but mostly empty arrays.
+        if new_length >= MIN_SPARSE_ARRAY_SIZE and isinstance(
+            array, DenseArrayProperties
+        ):
+            new_used_ratio = (array.elements_used + 1) / new_length
+            if new_used_ratio < MIN_DENSE_ARRAY_USED_RATIO:
+                # Switch to sparse array to avoid wasting space representing holes
+                self.array = SparseArrayProperties(
+                    entries=array.elements().items(), length=new_length
+                )
+                return
+        array.resize(new_length)
 
     def __delitem__(self, key: KT, /) -> None:
-        raise NotImplementedError
+        k = normalise_property_key(key)
+        if type(k) is str:
+            if k in self.properties:
+                del self.properties[k]
+                return
+            raise KeyError(key)  # preserve the non-normalised key
+        else:
+            assert isinstance(k, int)
+            array = self.array
+            # We model a dict and del behaviour for dict is quite different to
+            # list. We throw KeyError if the key is not set to a value in the
+            # array. We remove the key by assigning JSHole (del on the array
+            # shifts everything back by 1).
+            if k < len(array):
+                if self.array[k] is not JSHole:
+                    self.array[k] = JSHole
+                    return
+            raise KeyError(key)  # preserve the non-normalised key
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        return self.array.elements_used + len(self.properties)
 
     def __iter__(self) -> Iterator[KT]:
-        raise NotImplementedError
-
-    # TODO: should we provide a specialised map that acts like JavaScript
-    #   objects? e.g. allows for number or string keys that are synonyms. Plus
-    #   insertion ordering.
-    pass
+        it: Iterator[int | str] = chain(self.array.element_indexes(), self.properties)
+        return it  # type: ignore[return-value]
---
+++ test/jstypes/test_jsobject__statemachine.py
@@ -0,0 +1,302 @@
+from __future__ import annotations
+
+import pytest
+from hypothesis import strategies as st
+from hypothesis.stateful import (
+    RuleBasedStateMachine,
+    initialize,
+    invariant,
+    precondition,
+    rule,
+)
+
+from v8serialize.jstypes._normalise_property_key import normalise_property_key
+from v8serialize.jstypes.jsarrayproperties import (
+    MAX_ARRAY_LENGTH,
+    MIN_DENSE_ARRAY_USED_RATIO,
+    MIN_SPARSE_ARRAY_SIZE,
+    DenseArrayProperties,
+)
+from v8serialize.jstypes.jsobject import JSObject
+
+integer_name = st.one_of(
+    st.integers(max_value=-1), st.integers(min_value=MAX_ARRAY_LENGTH)
+)
+"""
+int values that are not valid as array indexes (due to being negative or too
+large).
+"""
+
+property_names = st.text().filter(lambda n: isinstance(normalise_property_key(n), str))
+"""
+JavaScript object property names that are not valid array indexes.
+"""
+
+array_indexes = st.integers(min_value=0, max_value=MAX_ARRAY_LENGTH - 1)
+usable_array_indexes = st.integers(min_value=0, max_value=200)
+
+
+class JSObjectComparisonMachine(RuleBasedStateMachine):
+    """
+    Verify the behaviour of JSObject by driving it through various
+    state-mutating actions and verifying the actual state matches a reference
+    model.
+    """
+
+    actual: JSObject[int | str, object]
+    reference_array: dict[int, object]
+    reference_properties: dict[str, object]
+
+    @initialize()
+    def init(self) -> None:
+        self.actual = JSObject()
+        self.reference_array = {}
+        self.reference_properties = {}
+
+    ############################
+    # State-dependant strategies
+
+    @property
+    def nonexistant_array_indexes(self) -> st.SearchStrategy[int]:
+        """
+        Get a strategy that generates array index values that are in the
+        array's length but empty, or above the array's range."""
+        return array_indexes.filter(lambda i: i not in self.reference_array.keys())
+
+    @staticmethod
+    def get_nonexistant_array_indexes(
+        self: JSObjectComparisonMachine,
+    ) -> st.SearchStrategy[int]:
+        return self.nonexistant_array_indexes
+
+    @property
+    def existant_array_indexes(self) -> st.SearchStrategy[int]:
+        """
+        Get a strategy that returns array index values that have existing values
+        in the JSObject array.
+        """
+        if len(self.reference_array) == 0:
+            return st.nothing()
+        return st.sampled_from(list(self.reference_array.keys()))
+
+    @staticmethod
+    def get_existant_array_indexes(
+        self: JSObjectComparisonMachine,
+    ) -> st.SearchStrategy[int]:
+        return self.existant_array_indexes
+
+    @property
+    def existant_property_names(self) -> st.SearchStrategy[str]:
+        """
+        Get a strategy that returns property name values that have existing
+        values in the JSObject properties.
+        """
+        if len(self.reference_properties) == 0:
+            return st.nothing()
+        return st.sampled_from(list(self.reference_properties.keys()))
+
+    @staticmethod
+    def get_existant_property_names(
+        self: JSObjectComparisonMachine,
+    ) -> st.SearchStrategy[str]:
+        return self.existant_property_names
+
+    @property
+    def nonexistant_property_names(self) -> st.SearchStrategy[str]:
+        """
+        Get a strategy that returns property name values that are not currently
+        set.
+        """
+        return property_names.filter(
+            lambda n: n not in self.reference_properties.keys()
+        )
+
+    @staticmethod
+    def get_nonexistant_property_names(
+        self: JSObjectComparisonMachine,
+    ) -> st.SearchStrategy[str]:
+        return self.nonexistant_property_names
+
+    #########################
+    # Precondition Predicates
+
+    @staticmethod
+    def array_not_empty(self: JSObjectComparisonMachine) -> bool:
+        return len(self.reference_array) > 0
+
+    @staticmethod
+    def properties_not_empty(self: JSObjectComparisonMachine) -> bool:
+        return len(self.reference_properties) > 0
+
+    ####################
+    # Rules: __getitem__
+
+    @rule(
+        index=st.runner().flatmap(get_nonexistant_array_indexes), via_str=st.booleans()
+    )
+    def getitem_array_non_existant(self, index: int, via_str: bool) -> None:
+        key: int | str = str(index) if via_str else index
+
+        assert index not in self.reference_array
+
+        with pytest.raises(KeyError) as exc_info:
+            self.actual[key]
+
+        assert exc_info.value.args[0] == key
+
+    @rule(index=st.runner().flatmap(get_existant_array_indexes), via_str=st.booleans())
+    @precondition(array_not_empty)
+    def getitem_array_existant(self, index: int, via_str: bool) -> None:
+        key: int | str = str(index) if via_str else index
+
+        assert index in self.reference_array
+        assert self.reference_array[index] == self.actual[key]
+
+    @rule(name=st.runner().flatmap(get_existant_property_names))
+    @precondition(properties_not_empty)
+    def getitem_property_existant(self, name: str) -> None:
+        assert name in self.reference_properties
+
+        assert self.reference_properties[name] == self.actual[name]
+
+        # Verify that names that are also ints are treated as name properties
+        try:
+            index = int(name)
+        except ValueError:
+            return
+        assert index not in self.reference_array
+        assert self.reference_properties[name] == self.actual[index]
+
+    @rule(name=st.runner().flatmap(get_nonexistant_property_names))
+    def getitem_properties_non_existant(self, name: str) -> None:
+        assert name not in self.reference_properties
+
+        with pytest.raises(KeyError) as exc_info:
+            self.actual[name]
+
+        assert exc_info.value.args[0] == name
+
+        # Verify that names that are also ints are treated as name properties
+        try:
+            index = int(name)
+        except ValueError:
+            return
+
+        with pytest.raises(KeyError) as exc_info:
+            self.actual[index]
+
+        assert exc_info.value.args[0] == index
+
+    ###################
+    # Rule: __setitem__
+
+    @rule(
+        index=usable_array_indexes,
+        via_str=st.booleans(),
+        value=st.integers(),
+    )
+    def setitem_array(self, index: int, via_str: bool, value: int) -> None:
+        key: int | str = str(index) if via_str else index
+        self.reference_array[index] = value
+        self.actual[key] = value
+
+    @rule(
+        name=integer_name,
+        via_str=st.booleans(),
+        value=st.integers(),
+    )
+    def setitem_property_non_index_int(
+        self, name: int, via_str: bool, value: int
+    ) -> None:
+        name_used: int | str = str(name) if via_str else name
+        self.reference_properties[str(name)] = value
+        self.actual[name_used] = value
+
+    @rule(
+        name=property_names,
+        value=st.integers(),
+    )
+    def setitem_property(self, name: str, value: int) -> None:
+        self.reference_properties[name] = value
+        self.actual[name] = value
+
+    ###################
+    # Rule: __delitem__
+
+    @rule(
+        index=st.runner().flatmap(get_nonexistant_array_indexes), via_str=st.booleans()
+    )
+    def delitem_array_non_existant(self, index: int, via_str: bool) -> None:
+        key: int | str = str(index) if via_str else index
+
+        assert index not in self.reference_array
+
+        with pytest.raises(KeyError) as exc_info:
+            del self.actual[key]
+
+        assert exc_info.value.args[0] == key
+
+    @rule(index=st.runner().flatmap(get_existant_array_indexes), via_str=st.booleans())
+    @precondition(array_not_empty)
+    def delitem_array_existant(self, index: int, via_str: bool) -> None:
+        key: int | str = str(index) if via_str else index
+
+        assert index in self.reference_array
+
+        del self.reference_array[index]
+        del self.actual[key]
+
+    @rule(name=st.runner().flatmap(get_existant_property_names))
+    @precondition(properties_not_empty)
+    def delitem_property_existant(self, name: str) -> None:
+        assert name in self.reference_properties
+
+        del self.reference_properties[name]
+        del self.actual[name]
+
+    @rule(name=st.runner().flatmap(get_nonexistant_property_names))
+    def delitem_properties_non_existant(self, name: str) -> None:
+        assert name not in self.reference_properties
+
+        with pytest.raises(KeyError) as exc_info:
+            del self.actual[name]
+
+        assert exc_info.value.args[0] == name
+
+    ############
+    # Invariants
+
+    @invariant()
+    def assert_jsobject_len_matches_reference(self) -> None:
+        assert len(self.actual) == len(self.reference_array) + len(
+            self.reference_properties
+        )
+
+    @invariant()
+    def assert_jsobject_array_matches_reference_array(self) -> None:
+        assert dict(self.actual.array.elements()) == self.reference_array
+
+    @invariant()
+    def assert_jsobject_properties_matches_reference_properties(self) -> None:
+        assert self.actual.properties == self.reference_properties
+
+    @invariant()
+    def assert_jsobject_iter_matches_reference(self) -> None:
+        assert list(self.actual) == [
+            # ascending numerical order
+            *sorted(self.reference_array.keys()),
+            # insertion order
+            *self.reference_properties.keys(),
+        ]
+
+    @invariant()
+    def assert_array_is_sparse_when_sparsely_populated(self) -> None:
+        array = self.actual.array
+        if (
+            len(array) >= MIN_SPARSE_ARRAY_SIZE
+            and len(self.reference_array) / len(array) < MIN_DENSE_ARRAY_USED_RATIO
+        ):
+            assert not isinstance(array, DenseArrayProperties)
+
+
+TestJSObjectComparison = JSObjectComparisonMachine.TestCase
