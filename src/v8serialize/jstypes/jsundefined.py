32e3c08e4700ddc84b84f0d0f35997617e05fc89
Hal Blackburn <hwtb2@cam.ac.uk>
2024-08-16 16:20:34 +0000

    feat: make JSUndefined an enum literal
    As with JSHole, using an Enum for this works better.

--- src/v8serialize/jstypes/jsundefined.py
+++ src/v8serialize/jstypes/jsundefined.py
@@ -1,10 +1,20 @@
-from typing import Final
+from enum import Enum
+from typing import Final, Literal, TypeAlias
 
 
-class JSUndefinedType:
-    def __init__(self) -> None:
-        if "JSUndefined" in globals():
-            raise AssertionError("Cannot instantiate JSUndefinedType")
+class JSUndefinedEnum(Enum):
+    """Defines the JSUndefined enum value."""
+
+    JSUndefined = "JSUndefined"
+    """Represents the JavaScript value `undefined`."""
+
+    def __repr__(self) -> str:
+        return self.name
+
+    def __str__(self) -> str:
+        return self.name
 
 
-JSUndefined: Final = JSUndefinedType()
+JSUndefinedType: TypeAlias = Literal[JSUndefinedEnum.JSUndefined]
+JSUndefined: Final = JSUndefinedEnum.JSUndefined
+"""Represents the JavaScript value `undefined`."""
