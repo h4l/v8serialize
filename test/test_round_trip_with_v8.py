6d1172179b5b7ddfdc63c1fa48008a1cab918f14
Hal Blackburn <hwtb2@cam.ac.uk>
2024-08-31 03:06:21 +0000

    test: conditionally enable V8-roundtrip features
    When testing round-tripping serialized data against the V8 echoserver,
    we now conditionally enable serialization features based on the V8
    version and platform of the server.

--- test/test_round_trip_with_v8.py
+++ test/test_round_trip_with_v8.py
@@ -1,16 +1,68 @@
-import os
+from __future__ import annotations
+
 from base64 import b64decode
 from dataclasses import dataclass
-from typing import Generator, Self
+from enum import Enum, StrEnum, auto
+from typing import Generator, Self, Sequence
 
 import httpx
 import pytest
-from hypothesis import given
+from hypothesis import given, settings
+from hypothesis import strategies as st
+from packaging.version import Version
 
-from v8serialize.decode import loads
-from v8serialize.encode import dumps
+from v8serialize.constants import SerializationFeature
+from v8serialize.decode import AnyTagMapper, TagMapper, loads
+from v8serialize.encode import (
+    DefaultEncodeContext,
+    Encoder,
+    ObjectMapper,
+    WritableTagStream,
+    dumps,
+)
+from v8serialize.extensions import node_js_array_buffer_view_host_object_handler
 
 from .strategies import any_object
+
+
+class V8Target(Enum):
+    Node18 = auto()
+    Node22 = auto()
+    Deno1_46 = auto()
+
+
+@dataclass
+class ReSerializationError:
+    name: ReSerializationErrorName
+    message: str
+    interpretation: str | None
+
+    @classmethod
+    def from_echo_result_json(cls, data: dict[object, object]) -> Self:
+        error_data = data.get("error")
+        if not isinstance(error_data, dict):
+            raise ValueError('"error" is not a dict')
+        raw_name = error_data.get("name")
+        if not (isinstance(raw_name, str) and raw_name in ReSerializationErrorName):
+            raise ValueError(
+                f'"error" "name" is not one of {ReSerializationErrorName}: {raw_name!r}'
+            )
+        name = ReSerializationErrorName(raw_name)
+        interpretation = error_data.get("interpretation")
+        if not (interpretation is None or isinstance(interpretation, str)):
+            raise ValueError(
+                f'"error" "interpretation" is not a string: {interpretation!r}'
+            )
+        message = error_data.get("message")
+        if not isinstance(message, str):
+            raise ValueError(f'"error" "message" is not a string: {message!r}')
+
+        return cls(name=name, message=message, interpretation=interpretation)
+
+
+class ReSerializationErrorName(StrEnum):
+    Deserialize = "deserialize-failed"
+    Serialize = "serialize-failed"
 
 
 @dataclass
@@ -55,23 +107,17 @@ class DeserializationError(ValueError):
 class V8SerializationEchoServerClient:
     httpclient: httpx.Client
     server_url: httpx.URL
+    v8_version: Version
 
-    def round_trip_serialized_value(self, serialized_value: bytes) -> ReSerializedValue:
+    def round_trip_serialized_value(
+        self, serialized_value: bytes
+    ) -> ReSerializedValue | ReSerializationError:
         try:
             resp = self.httpclient.post(
                 self.server_url,
                 content=serialized_value,
                 headers={"content-type": "application/x-v8-serialized"},
             )
-            if (
-                resp.status_code == 400
-                and "Unable to deserialize V8-serialized data" in resp.text
-            ):
-                raise DeserializationError(
-                    f"V8 serialization echo server was unable to deserialize "
-                    f"our data: {resp.text}",
-                    serialized_value,
-                )
             resp.raise_for_status
         except httpx.HTTPError as e:
             raise RuntimeError(
@@ -82,7 +128,10 @@ class V8SerializationEchoServerClient:
             echo_result = resp.json()
             if not isinstance(echo_result, dict):
                 raise ValueError("JSON body is not a dict")
-            return ReSerializedValue.from_echo_result_json(echo_result)
+            if echo_result.get("success"):
+                return ReSerializedValue.from_echo_result_json(echo_result)
+            else:
+                return ReSerializationError.from_echo_result_json(echo_result)
         except Exception as e:
             raise RuntimeError(
                 f"Failed to parse serialization echo response: {e}"
@@ -96,40 +145,120 @@ def httpclient() -> Generator[httpx.Client, None, None]:
 
 
 @pytest.fixture(autouse=True, scope="session")
-def check_v8_echoserver(httpclient: httpx.Client) -> None:
+def check_v8_echoserver(httpclient: httpx.Client, server_meta: EchoServerMeta) -> None:
     try:
-        resp = httpclient.get("http://localhost:8000/")
+        resp = httpclient.get(server_meta.server_url)
         resp.raise_for_status
     except httpx.HTTPError as e:
         raise RuntimeError(f"V8 echo server is not contactable: {e}") from e
 
 
-@pytest.fixture(scope="session")
-def server_url() -> httpx.URL:
-    raw_url = os.environ.get("V8CODEC_V8_ECHO_SERVER_URL") or "http://localhost:8000/"
+@dataclass
+class EchoServerMeta:
+    server_url: httpx.URL
+    v8_version: Version
+    extra_features: SerializationFeature
+
+
+@pytest.fixture(
+    scope="session",
+    params=[
+        pytest.param(
+            (
+                "http://v8serialize-echoserver-deno:8000/",
+                "12.9.202",
+                SerializationFeature.Float16Array,
+            ),
+            id="deno",
+        ),
+        pytest.param(
+            ("http://v8serialize-echoserver-node-22:8000/", "12.4.254", None),
+            id="node-22",
+        ),
+        pytest.param(
+            ("http://v8serialize-echoserver-node-18:8000/", "10.2.154", None),
+            id="node-18",
+        ),
+    ],
+)
+def server_meta(request: pytest.FixtureRequest) -> EchoServerMeta:
+    # raw_url = os.environ.get("V8CODEC_V8_ECHO_SERVER_URL") or "http://localhost:8000/"
+    raw_url, raw_version, extra_features = request.param
     try:
-        return httpx.URL(raw_url)
-    except Exception as e:
-        raise RuntimeError(
-            f"V8CODEC_V8_ECHO_SERVER_URL envar is not a valid URL. "
-            f"V8CODEC_V8_ECHO_SERVER_URL={raw_url!r}, error={e}"
+        return EchoServerMeta(
+            server_url=httpx.URL(raw_url),
+            v8_version=Version(raw_version),
+            extra_features=SerializationFeature(extra_features or 0),
         )
+    except Exception as e:
+        raise
+        # FIXME:
+        # raise RuntimeError(
+        #     f"V8CODEC_V8_ECHO_SERVER_URL envar is not a valid URL. "
+        #     f"V8CODEC_V8_ECHO_SERVER_URL={raw_url!r}, error={e}"
+        # )
 
 
 @pytest.fixture(scope="session")
 def echoclient(
-    httpclient: httpx.Client, server_url: httpx.URL
+    httpclient: httpx.Client, server_meta: EchoServerMeta
 ) -> V8SerializationEchoServerClient:
-    return V8SerializationEchoServerClient(httpclient, server_url)
+    return V8SerializationEchoServerClient(
+        httpclient=httpclient,
+        server_url=server_meta.server_url,
+        v8_version=server_meta.v8_version,
+    )
 
 
-@given(start_value=any_object(allow_theoretical=False))
+@pytest.fixture(scope="session")
+def features(server_meta: EchoServerMeta) -> SerializationFeature:
+    """The serialization features supported by the server's V8 version."""
+    assert (
+        server_meta.v8_version >= SerializationFeature.MaxCompatibility.first_v8_version
+    )
+
+    features = SerializationFeature.MaxCompatibility | server_meta.extra_features
+    for feature in SerializationFeature:
+        if feature.first_v8_version <= server_meta.v8_version:
+            features |= feature
+
+    return features
+
+
+# TODO: also test with serialize_object_references (default_object_mappers)
+object_mappers = [ObjectMapper()]
+tag_mappers: Sequence[AnyTagMapper] = [
+    TagMapper(host_object_deserializer=node_js_array_buffer_view_host_object_handler)
+]
+# @pytest.mark.parametrize("object_mappers", [[ObjectMapper()]])
+
+
+def get_any_object_strategy_for_v8target(
+    features: SerializationFeature,
+) -> st.SearchStrategy[object]:
+    return any_object(allow_theoretical=False, max_leaves=10, features=features)
+
+
+@settings(max_examples=1000, deadline=1000)
+@given(data=st.data())
 def test_codec_rt_object(
-    start_value: object, echoclient: V8SerializationEchoServerClient
+    data: st.DataObject,
+    echoclient: V8SerializationEchoServerClient,
+    features: SerializationFeature,
 ) -> None:
-    our_serialized_value = dumps(start_value)
+    start_value = data.draw(get_any_object_strategy_for_v8target(features))
+
+    # TODO: support feature flags in dumps()
+    encode_ctx = DefaultEncodeContext(
+        object_mappers=object_mappers, stream=WritableTagStream(features=features)
+    )
+    encode_ctx.stream.write_header()
+    encode_ctx.encode_object(start_value)
+    our_serialized_value = bytes(encode_ctx.stream.data)
+
     v8_result = echoclient.round_trip_serialized_value(our_serialized_value)
-    round_tripped_value = loads(v8_result.reserialized_value)
+    assert isinstance(v8_result, ReSerializedValue)
+    round_tripped_value = loads(v8_result.reserialized_value, tag_mappers=tag_mappers)
 
     if start_value != round_tripped_value:
         print(f"start_value: {start_value!r}")
