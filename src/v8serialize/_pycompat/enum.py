ef8cb7f261c6020b28bea91d0ea5eb03ff6bafbf
Hal Blackburn <hwtb2@cam.ac.uk>
2024-09-02 05:14:40 +0000

    chore: backport enum features we need for 3.9
    StrEnum and enum Flag values that are iterable.

---
+++ src/v8serialize/_pycompat/enum.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import sys
+from enum import Flag, IntFlag
+from typing import Iterator
+from typing_extensions import Self
+
+if sys.version_info < (3, 11):
+    from enum import Enum
+
+    class StrEnum(str, Enum):
+        def __str__(self) -> str:
+            assert self._value_ is not self
+            return str(self._value_)
+
+else:
+    from enum import StrEnum as StrEnum  # noqa: F401  # re-export
+
+
+if sys.version_info < (3, 10):
+
+    # Even though IntFlag is just (int, Flag), MyPy errors if we subclass
+    # (IterableFlag, IntFlag), so we have to redundantly define this for both
+    # types.
+    class IterableFlag(Flag):
+        def __iter__(self) -> Iterator[Self]:
+            for flag in type(self):
+                if self & flag:
+                    yield flag
+
+    class IterableIntFlag(IntFlag):
+        def __iter__(self) -> Iterator[Self]:
+            for flag in type(self):
+                if self & flag:
+                    yield flag
+
+else:
+
+    class IterableFlag(Flag):
+        pass
+
+    class IterableIntFlag(IntFlag):
+        pass
