47b2e31e7f5774c19e20f7bc45b30521c75c2818
Hal Blackburn <hwtb2@cam.ac.uk>
2024-09-04 01:12:19 +0000

    test: verify exception snapshots without colno
    On Python versions without exception traceback column numbers we don't
    allow generating snapshots, but still verify that the output without
    column numbers matches the pre-existing snapshots, ignoring column
    numbers.
    
    We also have to ignore the difference between
    `exceptiongroup.ExceptionGroup` and `ExceptionGroup` — the backport vs
    the builtin.

--- test/conftest.py
+++ test/conftest.py
@@ -1,18 +1,39 @@
 from __future__ import annotations
 
 import re
+import sys
+from enum import Enum, auto
 from pathlib import Path
 
 from pytest_insta import Fmt
 
 
+class ExceptionColno(Enum):
+    Supported = auto()
+    Unsupported = auto()
+
+    @staticmethod
+    def get_current() -> ExceptionColno:
+        return (
+            ExceptionColno.Supported
+            if sys.version_info >= (3, 11)
+            else ExceptionColno.Unsupported
+        )
+
+    @property
+    def is_current(self) -> bool:
+        return self == ExceptionColno.get_current()
+
+
 class FmtException(Fmt[str]):  # type: ignore[no-untyped-call] # __init_subclass__
     extension = ".exc.txt"
     project_dir: Path
+    colno: ExceptionColno
 
     def __init__(self) -> None:
         self.project_dir = Path(__file__, "../..").resolve()
         self.project_dir_pattern = re.escape(str(Path(__file__, "../..").resolve()))
+        self.colno = ExceptionColno.get_current()
 
     def with_absolute_paths(self, trace: str) -> str:
         return re.sub(r'(?<=[("])/…/v8serialize/', f"{self.project_dir}/", trace)
@@ -20,10 +41,31 @@ class FmtException(Fmt[str]):  # type: ignore[no-untyped-call] # __init_subclass
     def with_relative_paths(self, trace: str) -> str:
         return re.sub(f'(?<=[("]){self.project_dir_pattern}/', "/…/v8serialize/", trace)
 
+    def with_unknown_colno(self, trace: str) -> str:
+        return re.sub(
+            r"^(\s+at .* \(.*:\d+):(\d+)\)$",
+            r"\1:<unknown>)",
+            trace,
+            flags=re.MULTILINE,
+        )
+
     def load(self, path: Path) -> str:
         exception_trace = path.read_text()
-        return self.with_absolute_paths(exception_trace)
+        exception_trace = self.with_absolute_paths(exception_trace)
+        if self.colno is ExceptionColno.Unsupported:
+            exception_trace = self.with_unknown_colno(exception_trace)
+        return exception_trace
 
     def dump(self, path: Path, value: str) -> None:
         exception_trace = self.with_relative_paths(value)
+        if self.colno is ExceptionColno.Unsupported:
+            raise AssertionError(
+                f"""\
+Cannot dump a modified exception trace snapshot on a platform with \
+{self.colno}. You must (re)generate the snapshot on a platform that supports \
+column numbers in tracebacks (Python 3.11+) and then run tests on platforms \
+that lack column number support to verify the pre-generated snapshot \
+matches their behaviour (column numbers are ignored when comparing snapshots
+on platforms without column number support)."""
+            )
         path.write_text(exception_trace)
--- test/jstypes/test__v8_traceback.py
+++ test/jstypes/test__v8_traceback.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+import sys
 from test.jstypes._v8_traceback_error_fixtures import (
     ErrorScenario,
     call_and_capture_tbe,
@@ -16,17 +18,36 @@ FmtExc = Callable[[Callable[[], Never]], str]
 
 
 def call_and_format_exception_for_v8(fn: Callable[[], Never]) -> str:
-    return "".join(format_exception_for_v8(call_and_capture_tbe(fn)))
+    result = "".join(format_exception_for_v8(call_and_capture_tbe(fn)))
+
+    if sys.version_info < (3, 11):
+        # we use the exceptiongroup backport before 3.11, and its ExceptionGroup
+        # is prefixed with its module name in tracebacks. We remove the prefix
+        # to ignore this difference.
+        result = re.sub(r"\bexceptiongroup\.ExceptionGroup\b", "ExceptionGroup", result)
+
+    print(result)
+    return result
 
 
 def call_and_format_exception_for_python(fn: Callable[[], Never]) -> str:
-    return "".join(call_and_capture_tbe(fn).format())
+    result = "".join(call_and_capture_tbe(fn).format())
+    print(result)
+    return result
 
 
 @pytest.fixture(
     params=[
         pytest.param(call_and_format_exception_for_v8, id="v8"),
-        pytest.param(call_and_format_exception_for_python, id="py"),
+        pytest.param(
+            call_and_format_exception_for_python,
+            id="py",
+            marks=pytest.mark.skipif(
+                sys.version_info[:2] != (3, 12),
+                reason="py traceback generated on py3.12 only as a reference "
+                'for "correct" behaviour',
+            ),
+        ),
     ]
 )
 def fmt_exc(request: pytest.FixtureRequest) -> FmtExc:
