ed3c859dfb0070cff3d0d8da4f0df14c83847bf6
Hal Blackburn <hwtb2@cam.ac.uk>
2024-09-13 06:43:15 +0000

    feat: export core jstypes API from v8serialize.jstypes

--- src/v8serialize/jstypes/__init__.py
+++ src/v8serialize/jstypes/__init__.py
@@ -1,12 +1,46 @@
 from __future__ import annotations
 
-from v8serialize.jstypes._equality import JSSameValueZero as JSSameValueZero
-from v8serialize.jstypes._equality import same_value_zero as same_value_zero
+from v8serialize.constants import JSRegExpFlag as JSRegExpFlag
+from v8serialize.jstypes._repr import js_repr_settings as js_repr_settings
 from v8serialize.jstypes.jsarray import JSArray as JSArray
 from v8serialize.jstypes.jsarrayproperties import JSHole as JSHole
 from v8serialize.jstypes.jsarrayproperties import JSHoleType as JSHoleType
+from v8serialize.jstypes.jsbuffers import BaseJSArrayBuffer as BaseJSArrayBuffer
+from v8serialize.jstypes.jsbuffers import (
+    BoundsJSArrayBufferError as BoundsJSArrayBufferError,
+)
+from v8serialize.jstypes.jsbuffers import (
+    ByteLengthJSArrayBufferError as ByteLengthJSArrayBufferError,
+)
+from v8serialize.jstypes.jsbuffers import DataViewBuffer as DataViewBuffer
+from v8serialize.jstypes.jsbuffers import (
+    ItemSizeJSArrayBufferError as ItemSizeJSArrayBufferError,
+)
+from v8serialize.jstypes.jsbuffers import JSArrayBuffer as JSArrayBuffer
+from v8serialize.jstypes.jsbuffers import JSArrayBufferError as JSArrayBufferError
+from v8serialize.jstypes.jsbuffers import JSArrayBufferTransfer as JSArrayBufferTransfer
+from v8serialize.jstypes.jsbuffers import JSArrayBufferView as JSArrayBufferView
+from v8serialize.jstypes.jsbuffers import JSBigInt64Array as JSBigInt64Array
+from v8serialize.jstypes.jsbuffers import JSBigUint64Array as JSBigUint64Array
+from v8serialize.jstypes.jsbuffers import JSDataView as JSDataView
+from v8serialize.jstypes.jsbuffers import JSFloat16Array as JSFloat16Array
+from v8serialize.jstypes.jsbuffers import JSFloat32Array as JSFloat32Array
+from v8serialize.jstypes.jsbuffers import JSFloat64Array as JSFloat64Array
+from v8serialize.jstypes.jsbuffers import JSInt8Array as JSInt8Array
+from v8serialize.jstypes.jsbuffers import JSInt16Array as JSInt16Array
+from v8serialize.jstypes.jsbuffers import JSInt32Array as JSInt32Array
+from v8serialize.jstypes.jsbuffers import JSSharedArrayBuffer as JSSharedArrayBuffer
+from v8serialize.jstypes.jsbuffers import JSTypedArray as JSTypedArray
+from v8serialize.jstypes.jsbuffers import JSUint8Array as JSUint8Array
+from v8serialize.jstypes.jsbuffers import JSUint8ClampedArray as JSUint8ClampedArray
+from v8serialize.jstypes.jsbuffers import JSUint16Array as JSUint16Array
+from v8serialize.jstypes.jsbuffers import JSUint32Array as JSUint32Array
+from v8serialize.jstypes.jsbuffers import get_buffer as get_buffer
+from v8serialize.jstypes.jserror import JSError as JSError
+from v8serialize.jstypes.jserror import JSErrorData as JSErrorData
 from v8serialize.jstypes.jsmap import JSMap as JSMap
 from v8serialize.jstypes.jsobject import JSObject as JSObject
+from v8serialize.jstypes.jsprimitiveobject import JSPrimitiveObject as JSPrimitiveObject
 from v8serialize.jstypes.jsregexp import JSRegExp as JSRegExp
 from v8serialize.jstypes.jsset import JSSet as JSSet
 from v8serialize.jstypes.jsundefined import JSUndefined as JSUndefined
