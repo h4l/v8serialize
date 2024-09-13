d4001a129eb280ffa365fe40d86fa01e32c5feb6
Hal Blackburn <hwtb2@cam.ac.uk>
2024-09-13 06:21:03 +0000

    feat: export core user API from v8serialize package

--- src/v8serialize/__init__.py
+++ src/v8serialize/__init__.py
@@ -1,1 +1,20 @@
 from __future__ import annotations
+
+from v8serialize.constants import SerializationFeature as SerializationFeature
+from v8serialize.decode import default_tag_mappers as default_tag_mappers
+from v8serialize.decode import loads as loads
+from v8serialize.encode import EncodeV8CodecError as EncodeV8CodecError
+from v8serialize.encode import (
+    FeatureNotEnabledEncodeV8CodecError as FeatureNotEnabledEncodeV8CodecError,
+)
+from v8serialize.encode import (
+    UnmappedValueEncodeV8CodecError as UnmappedValueEncodeV8CodecError,
+)
+from v8serialize.encode import default_object_mappers as default_object_mappers
+from v8serialize.encode import dumps as dumps
+from v8serialize.errors import DecodeV8CodecError as DecodeV8CodecError
+from v8serialize.errors import JSRegExpV8CodecError as JSRegExpV8CodecError
+from v8serialize.errors import (
+    UnmappedTagDecodeV8CodecError as UnmappedTagDecodeV8CodecError,
+)
+from v8serialize.errors import V8CodecError as V8CodecError
