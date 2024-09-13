1125049492e1a2816c2cc5e9b07afea6375ff6c6
Hal Blackburn <hwtb2@cam.ac.uk>
2024-09-13 02:49:32 +0000

    chore: support subscripting dataclass-classes on py310
    Python 3.10 seems to have a bug/regression that causes MyType[XXX]() to
    fail when MyType is a frozen dataclass. `FrozenAfterInitDataclass`
    avoids this problem, while also making frozen dataclass init more
    user-friendly. Rather than blocking setattr entirely, it allows
    dataclass attrs to be set once, then not again subsequently. It also
    only freezes dataclass fields, not other dunder fields, which allows
    typing.Generic to modify the generic instances as it desires.

--- src/v8serialize/_pycompat/dataclasses.py
+++ src/v8serialize/_pycompat/dataclasses.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import sys
+from dataclasses import FrozenInstanceError, dataclass
+from dataclasses import fields as dataclass_fields
 from typing import Literal, TypedDict
 
 
@@ -21,3 +23,24 @@ else:
 
     def slots_if310() -> SlotsTrue:
         return SlotsTrue(slots=True)
+
+
+@dataclass
+class FrozenAfterInitDataclass:
+    """A mixin for dataclasses that disallows changing fields after init.
+
+    Fields can be set once and not again. This is an alternative to
+    `@dataclass(frozen=True)` — it only freezes dataclass-managed fields, so it
+    doesn't affect non-dataclass fields, such as typing.Generic's dunder fields.
+    """
+
+    def __delattr__(self, name: str) -> None:
+        if name in (f.name for f in dataclass_fields(self)):
+            raise FrozenInstanceError(f"cannot delete field {name}")
+        super(FrozenAfterInitDataclass, self).__delattr__(name)
+
+    def __setattr__(self, name: str, value: object) -> None:
+        if name in (f.name for f in dataclass_fields(self)):
+            if hasattr(self, name):
+                raise FrozenInstanceError(f"cannot set {name!r}")
+        super(FrozenAfterInitDataclass, self).__setattr__(name, value)
---
+++ test/pycompat/test_dataclasses.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import sys
+from dataclasses import FrozenInstanceError, dataclass
+from typing import Generic, TypeVar
+
+import pytest
+
+from v8serialize._pycompat.dataclasses import FrozenAfterInitDataclass, slots_if310
+
+T = TypeVar("T")
+
+
+@pytest.mark.skipif(
+    sys.version_info[:2] != (3, 10), reason="Test applies only to py310"
+)
+def test_frozen_generic_dataclass() -> None:
+    @dataclass(frozen=True, slots=True)
+    class BrokenOn310(Generic[T]):
+        foo: T
+
+    # Using the class directly works
+    assert BrokenOn310(foo="")
+
+    # Subscripting with a type does not
+    with pytest.raises(
+        TypeError,
+        match=r"super\(type, obj\): obj must be an instance or subtype of type",
+    ):
+        BrokenOn310[str](foo="")
+
+    @dataclass(slots=True)
+    class OKOn310(FrozenAfterInitDataclass, Generic[T]):
+        foo: T
+
+    assert OKOn310[str](foo="")
+
+
+def test_FrozenAfterInitDataclass() -> None:
+    @dataclass(unsafe_hash=True, **slots_if310())
+    class Example(FrozenAfterInitDataclass):
+        a: int
+
+    e = Example(a=1)
+    assert e.a == 1
+
+    with pytest.raises(FrozenInstanceError):
+        e.a = 2
+
+    with pytest.raises(FrozenInstanceError):
+        del e.a
+
+    assert Example(a=1) == Example(a=1)
+    assert hash(Example(a=1)) == hash(Example(a=1))
+
+    # No need for object.__setattr__ to initialise frozen fields
+    @dataclass(init=False)
+    class Example2(Example):
+        b: str
+
+        def __init__(self, a: int) -> None:
+            super().__init__(a)
+            self.b = str(a)
+
+    ex2 = Example2(10)
+    assert ex2.a == 10
+    assert ex2.b == "10"
