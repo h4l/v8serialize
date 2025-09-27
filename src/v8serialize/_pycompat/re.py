14ebcc64e37c8e36323fbc3983249a0420fe5227
Hal Blackburn <hwtb2@cam.ac.uk>
2025-09-27 07:17:24 +0000

    chore: update pycompat RegexFlag comment for 3.10

--- src/v8serialize/_pycompat/re.py
+++ src/v8serialize/_pycompat/re.py
@@ -11,7 +11,8 @@ from re import VERBOSE as VERBOSE
 from v8serialize._pycompat.enum import IterableIntFlag
 
 
-# 3.9 doesn't define NOFLAG, and default IntFlag is not iterable in 3.9.
+# 3.10 doesn't define NOFLAG, and default IntFlag is not iterable in 3.9, 3.10.
+# (It actually is iterable in 3.10, but is still typed as if it's not.)
 class RegexFlag(IterableIntFlag):
     NOFLAG = 0
     ASCII = ASCII
